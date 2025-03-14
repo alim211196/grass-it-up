e78a7d6cd120e4d73dc2a574f3e7e82ed37d65cb
Alim-Oxcytech <alim.mohd@oxcytech.com>
2025-03-14 16:25:25 +0530

    remove coming soon

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import HeroBanner from "./sections/HeroBanner";
 import Marquee from "./sections/Marquee";
-import "./App.css";
 import ProductGrid from "./sections/ProductGrid";
 import BreakerSection from "./sections/BreakerSection";
 import InformativeCategorySection from "./sections/InformativeCategorySection";
@@ -11,9 +10,6 @@ function App() {
     <>
       <HeroBanner />
       <Marquee />
-      <section className={"placeholderSection"}>
-        <h2>Coming Soon</h2>
-      </section>
       <InformativeCategorySection />
       <ProductGrid />
       <BreakerSection />
