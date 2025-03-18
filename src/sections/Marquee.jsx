5dc7cd8bb8c97125306000b73c5963fbb9650961
Alim-Oxcytech <alim.mohd@oxcytech.com>
2025-03-18 12:11:08 +0530

    faded 2

--- src/sections/Marquee.jsx
+++ src/sections/Marquee.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 const Marquee = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
   return (
     <div className={"marqueeContainer"}>
-      <div className={"marqueeContent"}>
+      <div
+        className={"marqueeContent"}
+        data-aos="fade-right"
+        data-aos-delay="100"
+      >
         <span>
           <i class="fas fa-bolt"></i> Spring Clearance Event: Save Up to 70%
           &nbsp;
