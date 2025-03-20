87d6abc2bc65db4fe21381ce1d647b07054d4be9
Alim-Oxcytech <alim.mohd@oxcytech.com>
2025-03-21 00:33:41 +0530

    fixed issue

--- src/containers/ShopByProduct/index.jsx
+++ src/containers/ShopByProduct/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./ShopByProduct.css";
+import "./shopByProduct.css";
 import Header from "../HomePage/subComponents/Header";
 import { useNavigate } from "react-router-dom";
 
