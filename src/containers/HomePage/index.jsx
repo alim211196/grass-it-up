3563b931eaff07e740408e1e336e6d14f8953b0c
Alim-Oxcytech <alim.mohd@oxcytech.com>
2025-03-25 10:22:20 +0530

    remove loader

--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -42,7 +42,7 @@ const HomePage = () => {
       <HorizontalScroll />
       <Chatbot />
       {/* Lazy-loaded components wrapped in Suspense */}
-      <Suspense fallback={<SuspenseLoader />}>
+      <Suspense fallback={null}>
         <InformativeCategorySection />
         <ProductGrid />
         <BreakerSection />
